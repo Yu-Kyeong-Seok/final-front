166607658525bebf3dd8c40a62c10ad270d05459
dayoungh <dayeong1024@gmail.com>
2024-11-08 12:35:41 +0900

    [황다영] conflict 수정

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -2,29 +2,33 @@ import React from "react";
 import { InputBaseProps, InputFullProps } from "./Input.type";
 import InputStyled from "./Input.styled";
 
-
 const Input = (props: InputFullProps) => {
-    const { color, borderColor, placeholder } = props;
-    return (
-        <InputStyled.Wrapper color = {color} borderColor = {borderColor} placeholder={placeholder} type="text"></InputStyled.Wrapper>
-    )
-}
+  const { color, borderColor, placeholder } = props;
+  return (
+    <InputStyled.Wrapper
+      color={color}
+      borderColor={borderColor}
+      placeholder={placeholder}
+      type="text"
+    ></InputStyled.Wrapper>
+  );
+};
 
 export const InputStyle1 = (props: InputBaseProps) => {
-    const { placeholder } = props;
-    return (
-        <Input 
-        color={"var(--color-800)"}
-        borderColor={"var(--color-100)"}
-        placeholder={placeholder}
+  const { placeholder } = props;
+  return (
+    <Input
+      color={"var(--color-800)"}
+      borderColor={"var(--color-100)"}
+      placeholder={placeholder}
     />
-    );
-}
+  );
+};
 
 // export const InputStyle2 = (props: InputBaseProps) => {
 //     const { placeholder } = props;
 //     return (
-//         <Input 
+//         <Input
 //         color={"var(--color-200)"}
 //         borderColor={"var(--color-800)"}
 //         placeholder={placeholder}
@@ -35,7 +39,7 @@ export const InputStyle1 = (props: InputBaseProps) => {
 // export const InputStyle3 = (props: InputBaseProps) => {
 //     const { placeholder } = props;
 //     return (
-//         <Input 
+//         <Input
 //         color={"var(--color-800)"}
 //         borderColor={"var(--color-800)"}
 //         placeholder={placeholder}
