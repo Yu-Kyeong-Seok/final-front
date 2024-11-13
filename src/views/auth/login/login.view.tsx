f220a9706ef200e545a7c35e641164076533bd9e
GyeonghoJeon <wjstkwal7@gmail.com>
2024-11-13 17:50:45 +0900

    [전경호] 회원가입 기능 작업 완료

--- src/views/auth/login/login.view.tsx
+++ src/views/auth/login/login.view.tsx
@@ -4,6 +4,7 @@ import styles from "./login.view.module.scss";
 import cn from "classnames/bind";
 import { Controller, useForm } from "react-hook-form";
 import Button from "@/src/components/Button/Button";
+import CheckBoxView from "@/src/components/CheckBox/CheckBox.view";
 
 const cx = cn.bind(styles);
 
@@ -16,8 +17,8 @@ const LoginView = () => {
                     <Input type="password" placeholder="비밀번호를 입력해주세요"></Input>
                 </div>
                 <div className={cx("InnerContainer")}>
-                    <Button variants="solid" text="로그인"></Button>
-                    <Button variants="outline" text="회원가입"></Button>
+                    <Button variants="solid"><span>로그인</span></Button>
+                    <Button variants="outline"><span>회원가입</span></Button>
                 </div>
                 <div className={cx("SearchContainer")}>
                     <a>아이디 찾기</a>
