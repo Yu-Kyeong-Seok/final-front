1160c68e98396da7804153ac0e026ece88c91871
dayoungh <dayeong1024@gmail.com>
2024-11-13 11:59:10 +0900

    merge

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,22 +5,26 @@ import cn from "classnames/bind";
 const cx = cn.bind(styles);
 
 type ButtonProps = {
-    text?: string;
-    disabled?: boolean;
-    variants?: "solid" | "outline"; 
-    onClick?: any;
-    type?: "submit" | "reset" | "button" | undefined;
-    className?: string;
-}
+  text?: string;
+  disabled?: boolean;
+  variants?: "solid" | "outline";
+  onClick?: any;
+  type?: "submit" | "reset" | "button" | undefined;
+  className?: string;
+};
 
 const Button = (props: ButtonProps) => {
-    const { text, disabled, variants, onClick, type} = props;
-    return (
-        <button className={cx("Button", className, {[`${variants}`]: variants})} disabled={disabled} onClick={onClick} type={type}>
-            <span className={cx("span")}>{text}</span>
-        </button>
-
-    );
+  const { text, disabled, variants, onClick, type, className } = props;
+  return (
+    <button
+      className={cx("Button", className, { [`${variants}`]: variants })}
+      disabled={disabled}
+      onClick={onClick}
+      type={type}
+    >
+      <span className={cx("span")}>{text}</span>
+    </button>
+  );
 };
 
 export default Button;
