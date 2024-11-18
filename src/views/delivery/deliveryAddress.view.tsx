edce2ff5a3dd5f80679f1d0a75beefe5a40fb81e
dayoungh <dayeong1024@gmail.com>
2024-11-18 20:27:23 +0900

    [황다영]배송지관리페이지

--- src/views/delivery/deliveryAddress.view.tsx
+++ src/views/delivery/deliveryAddress.view.tsx
@@ -4,13 +4,29 @@ import CheckBox from "@/src/components/CheckBox/CheckBox";
 import styles from "./deliveryAddress.module.scss";
 import cn from "classnames/bind";
 // import { useState } from "react";
-import { LuChevronLeft, LuAlertCircle } from "react-icons/lu";
+import { LuChevronLeft, LuAlertCircle, LuPencil } from "react-icons/lu";
 import { useState } from "react";
 // import { useRouter } from "next/navigation";
 
 const cx = cn.bind(styles);
 
-export default function DeliveryAddressView() {
+/** 주문자 정보 */
+type UserInfo = {
+  id: string;
+  name: string;
+  phone: string;
+  email: string;
+  address: string;
+};
+
+type OrderViewProps = {
+  // orderItems: OrderItem[];
+  userInfos: UserInfo[];
+};
+
+export default function DeliveryAddressView(props: OrderViewProps) {
+  const { userInfos } = props;
+
   const [selectedValues, setSelectedValues] = useState<string[]>([]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -36,12 +52,27 @@ export default function DeliveryAddressView() {
       </span>
 
       <section>
-        <CheckBox
-          onChange={handleChange}
-          value={selectedValues}
-          id="checkbox1"
-          name="checkbox-group"
-        />
+        <div className={cx("AddressList")}>
+          <CheckBox
+            onChange={handleChange}
+            value={selectedValues}
+            id="checkbox1"
+            name="checkbox-group"
+          />
+          <ul>
+            <li className={cx("AddressItem")}>{userInfos[0].address}</li>
+            <li>
+              <ul className={cx("AddressUser")}>
+                <li>{userInfos[0].name}</li>
+                <li>{userInfos[0].phone}</li>
+              </ul>
+            </li>
+            <li className={cx("DeliveryType")}>샛별배송</li>
+          </ul>
+          <span>
+            <LuPencil />
+          </span>
+        </div>
       </section>
     </div>
   );
