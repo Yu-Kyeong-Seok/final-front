d53b26e69cf294b95c75ac0f2bcd2a430a9cf98d
dayoungh <dayeong1024@gmail.com>
2024-11-19 09:54:06 +0900

    [황다영]칩스 컴포넌트 수정

---
+++ app/chips/page.tsx
@@ -0,0 +1,9 @@
+import ChipsView from "@/src/components/Chips/Chips.view";
+
+export default function ChipsPage() {
+  return (
+    <div>
+      <ChipsView />
+    </div>
+  );
+}
--- src/components/Chips/Chip.tsx
+++ src/components/Chips/Chip.tsx
@@ -6,12 +6,13 @@ const cx = cn.bind(styles);
 type ChipProps = {
   label: string;
   onClick: (label: string) => void;
+  className?: string;
 };
 
 const Chip = (props: ChipProps) => {
-  const { label, onClick } = props;
+  const { label, onClick, className } = props;
   return (
-    <div onClick={() => onClick(label)} className={cx("ChipText")}>
+    <div onClick={() => onClick(label)} className={cx("ChipText", className)}>
       {label}
     </div>
   );
