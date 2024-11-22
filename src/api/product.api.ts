5bc90746c2312a94ffc6404dd6db1b3fb67df6f8
ehddk <bj9147@naver.com>
2024-11-22 22:33:00 +0900

    [김동아] 검색 부분 마크업 및 API 작업

--- src/api/product.api.ts
+++ src/api/product.api.ts
@@ -21,6 +21,23 @@ export interface TransformedProduct extends Omit<Product, '_id' | 'productName' 
     salePrice: number;   // sales를 salePrice로 변환
 }
 
+export const fetchProducts = async () => {
+    try {
+        const response = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + '/api/products');
+        const data = await response.json();
+        const transformedData = data.map((item: Product): TransformedProduct => ({
+            ...item,
+            id: item._id,
+            name: item.productName,
+            salePrice: item.sales,
+        }));
+
+        return transformedData;
+    } catch (error) {
+        console.error("Error fetching product data:", error);
+        throw(error)
+    }
+}
 export const fetchProductsByCategory = async (mainCategory: string, subCategory: string) => {
     try {
         const response = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + '/api/products');
@@ -44,3 +61,30 @@ export const fetchProductsByCategory = async (mainCategory: string, subCategory:
         throw error;
     }
 };
+
+export const searchProducts=async (keyword:string)=>{
+    try{  
+           const encodedKeyword = encodeURIComponent(keyword);
+           const response = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + `/api/products/search?keyword=${encodedKeyword}`);
+    
+           if (!response.ok) {
+               const errorText = await response.text();
+               console.error('에러 응답:', errorText);
+               throw new Error(`API 요청 실패: ${response.status}`);
+           }
+   
+           const data = await response.json();
+           const transformedData = data.map((item: Product): TransformedProduct => ({
+               ...item,
+               id: item._id,
+               name: item.productName,
+               salePrice: item.sales,
+           }));
+          // console.log('제품,,',transformedData)
+           return transformedData;
+       } catch (error) {
+           console.error("제품 검색 실패:", error);
+           throw error;
+       }
+   
+}
